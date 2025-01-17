051912712e0e0ca38a10a2ee7731f32093271b6a
sushantaher <ahersuahnt@gmail.com>
2025-01-17 09:49:20 +0530

    finalcicdpipeline2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Final CICD pipeline for nodejs app ');
+ res.send('Final CICD pipeline for nodejs app i am happy this works');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
